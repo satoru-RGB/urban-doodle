9c33fe5036f1f7af7072154a2d6098ab1ad775b2
こいう <congbeicun184@gmail.com>
2025-06-16 01:35:09 +0900

    Add files via upload

---
+++ PictBookReader/test_app.py
@@ -0,0 +1,14 @@
+#test_app.py
+
+def test_example():
+    """
+    pytestが検出できる最も基本的なテストの例。
+    必ず成功する。
+    """
+    assert 1 == 1
+
+def test_application_can_run():
+    """
+    今後、アプリケーションの起動などをテストするための雛形。
+    """
+    assert True
